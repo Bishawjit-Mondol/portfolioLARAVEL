d34b85c1992173548e385c24f1575204bfd2388b
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2021-04-16 01:57:39 +0600

    first commit

---
+++ app/Http/Controllers/Demo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Demo extends Controller
+{
+    //
+    public function ui(){
+        return view('welcome');
+    }
+}
+
+
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Demo;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TemplateController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/w', function () {
+    return view('welcome');
+});
+
+Route::get('/w2', [Demo::class, 'ui']);
+
+
+Route::get('/me', [TemplateController::class, 'index']);
+
+
+
+// Route::get('/t', function () {
+//     return view('FrontEnd.home');
+// });
